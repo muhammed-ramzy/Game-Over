8eb1c503e4349341c5efeb16748bd3196f6323e5
muhammed-ramzy <muhammedabdelfattah89@gmail.com>
2024-06-29 15:57:21 +0300

    filtered added

--- JS/index.js
+++ JS/index.js
@@ -7,7 +7,16 @@ import { addCardlisteners } from "./games.js"
 //HTML Elements
 const displayed_games = document.getElementById("displayed_games");
 export const game_details = document.getElementById("game_details")
-export const games = document.getElementById("games")
+export const games = document.getElementById("games");
+const categories = document.querySelectorAll("#nav_item");
+let category = "mmorpg";
+
+for (let i = 0; i < categories.length; i++) {
+    categories[i].addEventListener("click", () =>{
+        category = categories[i].getAttribute("data-category");
+    })
+    
+}
 
 //instanciated objects
 const display = new UI();
@@ -20,7 +29,7 @@ let gamesData = [];
 
 // Games API
 async function getGamesData() {
-    const url = 'https://free-to-play-games-database.p.rapidapi.com/api/games?category=shooter';
+    const url = `https://free-to-play-games-database.p.rapidapi.com/api/games?category=${category}`;
 
     const options = {
         method: 'GET',
